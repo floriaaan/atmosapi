dc209ddf5c7d369445d1ae38ecbe2d2ed17ddd97
Florian Leroux <florian.leroux3@laposte.net>
2020-01-16 15:57:32 +0100

    Release Candidate RC2.1

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,5 +1,5 @@
-#sensor ESP8206-01 script
-
+# sensor ESP8206-01 script
+import json
 import urequests
 import random
 import time
@@ -10,23 +10,28 @@ import machine
 
 DEFAULT_I2C_ADDR = 0x27
 
-i2C_c = machine.I2C(-1 ,machine.Pin(2), machine.Pin(0))
+i2C_c = machine.I2C(-1, machine.Pin(2), machine.Pin(0))
 i2C_l = I2C(scl=Pin(2), sda=Pin(0))
 temp_sensor = si7021.Si7021(i2C_c)
 lcd = I2cLcd(i2C_l, DEFAULT_I2C_ADDR, 2, 16)
 
 
 def http_post(probe_id, temp, humidity):
-    url = 'http://localhost:5000/atmos/measure/add/'
-    data = {
-        'probe_id' : probe_id,
-        'temp' : temp,
-        'humidity' : humidity
+    url = 'http://floriaaan.alwaysdata.net/atmos/measure/add/'
+    datas = {
+        'probe_id': probe_id,
+        'temp': temp,
+        'humidity': humidity
     }
     headers = {
-        'charset': 'utf-8'
+        'charset': 'utf-8',
+        'Access-Control-Allow-Headers': '*'
     }
-    resp = urequests.post(url, json=data, headers=headers)
+    resp = urequests.post(url, data=json.dumps(datas), headers=headers)
+    lcd.clear()
+    lcd.putstr("Requête envoyée")
+    time.sleep(2)
+
 
 def moyenneTemp():
     var1 = temp_sensor.temperature
@@ -39,12 +44,12 @@ def moyenneTemp():
     time.sleep(1)
     var5 = temp_sensor.temperature
     time.sleep(1)
-    moy_temp=(var1 + var2 + var3 + var4 + var5) / 5
-    moy_temparr=round(moy_temp,2)
+    moy_temp = (var1 + var2 + var3 + var4 + var5) / 5
+    moy_temparr = round(moy_temp, 2)
     lcd.clear()
     lcd.putstr("Temperature :\n " + str(moy_temparr) + " C")
     return moy_temparr
-    
+
 
 def moyenneHumid():
     var1 = temp_sensor.relative_humidity
@@ -57,13 +62,14 @@ def moyenneHumid():
     time.sleep(1)
     var5 = temp_sensor.relative_humidity
     time.sleep(1)
-    
-    moy_humi=(var1 + var2 + var3 + var4 + var5) / 5
-    moy_humiarr=round(moy_humi,2)
+
+    moy_humi = (var1 + var2 + var3 + var4 + var5) / 5
+    moy_humiarr = round(moy_humi, 2)
     lcd.clear()
-    lcd.putstr("Humidite :\n " + str(moy_humiarr) +" %")
-    
+    lcd.putstr("Humidite :\n " + str(moy_humiarr) + " %")
+
     return moy_humiarr
+
 
 def sensor_dispsend():
     while True:
@@ -73,14 +79,17 @@ def sensor_dispsend():
 
             http_post(1, moy_temp, moy_humi)
         except KeyboardInterrupt:
-            lcd.putstr("KeyboardInterrupt!")
+            lcd.clear()
+            lcd.putstr("Keyboard\nInterrupt!")
             exit()
         except:
+            lcd.clear()
             print("!!!ERROR!!!")
             lcd.putstr("!!!ERROR!!!")
 
         time.sleep(10)
-    
+
+
 def sensor_disp():
     while True:
         try:
@@ -88,11 +97,12 @@ def sensor_disp():
             moy_humi = moyenneHumid()
 
         except KeyboardInterrupt:
-            lcd.putstr("KeyboardInterrupt!")
+            lcd.clear()
+            lcd.putstr("Keyboard\nInterrupt!")
             exit()
         except:
+            lcd.clear()
             print("!!!ERROR!!!")
             lcd.putstr("!!!ERROR!!!")
-        
+
         time.sleep(3)
-        
